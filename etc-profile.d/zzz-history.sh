0bc7fdebb751cbd84a6dcb9139459ddefd842982
Marko Rizvic <marko.rizvic@gmail.com>
2014-10-29 09:25:16 +0100

    added .bashrc and etc-profile.d

---
+++ etc-profile.d/zzz-history.sh
@@ -0,0 +1,9 @@
+shopt -s histappend
+shopt -s checkwinsize
+
+HISTCONTROL="ignoredups"
+unset HISTFILESIZE
+HISTSIZE="99999999999"
+HISTTIMEFORMAT="[%Y/%m/%d %H/%M/%S] "
+
+export HISTCONTROL HISTSIZE HISTTIMEFORMAT
