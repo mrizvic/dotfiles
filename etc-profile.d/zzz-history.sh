3df202fb3ed70db5f1ff06701a85ce8f7112dd44
Marko Rizvic <1878580+mrizvic@users.noreply.github.com>
2022-01-04 09:23:21 +0100

    Update zzz-history.sh

--- etc-profile.d/zzz-history.sh
+++ etc-profile.d/zzz-history.sh
@@ -4,6 +4,6 @@ shopt -s checkwinsize
 HISTCONTROL="ignoredups"
 unset HISTFILESIZE
 HISTSIZE="99999999999"
-HISTTIMEFORMAT="[%Y/%m/%d %H/%M/%S] "
+HISTTIMEFORMAT="[%Y/%m/%d %H:%M:%S] "
 
 export HISTCONTROL HISTSIZE HISTTIMEFORMAT
