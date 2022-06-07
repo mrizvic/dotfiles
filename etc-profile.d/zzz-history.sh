da9a2b583568c6948902a38222fbbb49dad8cb51
Marko Rizvic <1878580+mrizvic@users.noreply.github.com>
2022-06-07 21:04:18 +0200

    Update zzz-history.sh

--- etc-profile.d/zzz-history.sh
+++ etc-profile.d/zzz-history.sh
@@ -1,9 +1,8 @@
+# When the shell exits, append to the history file instead of overwriting it
 shopt -s histappend
+# # If set, Bash checks the window size after each command and, if necessary, updates the values of LINES and COLUMNS.
 shopt -s checkwinsize
-
-HISTCONTROL="ignoredups"
-unset HISTFILESIZE
+HISTCONTROL="ignoreboth"
 HISTSIZE="99999999999"
-HISTTIMEFORMAT="[%Y/%m/%d %H:%M:%S] "
-
+HISTTIMEFORMAT="[%Y-%m-%d %T] [$(logname)] "
 export HISTCONTROL HISTSIZE HISTTIMEFORMAT
