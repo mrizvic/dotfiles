cc256dd091c0573d82d3440da3f32e789457979d
Marko Rizvic <1878580+mrizvic@users.noreply.github.com>
2022-06-07 21:05:04 +0200

    Update ps1prompt.sh

--- etc-profile.d/ps1prompt.sh
+++ etc-profile.d/ps1prompt.sh
@@ -1,1 +1,10 @@
-
+### PROMPT
+if [ "$EUID" -ne 0 ]; then
+    DELIMITER="\$ "
+    COLOR="[1;32m\]"
+else
+    DELIMITER="# "
+    COLOR="[1;31m\]"
+fi
+PS1="\[\e[1;33m\]\w\n\[\e$COLOR\h\[\e[m\]$DELIMITER"
+export PS1
